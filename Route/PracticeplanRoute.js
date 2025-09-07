ccb54a744e01e1f14e4d174f60e98a40024c26cd
clinto98 <clinto@finlytyx.com>
2025-09-07 17:44:51 +0530

    missed

--- Route/PracticeplanRoute.js
+++ Route/PracticeplanRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  checkAnswerById, createPraticePlan, getPracticePlanQuestions, getQuestionBySectionAndIndex, getRandomQuestion } from "../Controller/PracticeplanController.js";
+import {  checkAnswerById, createPraticePlan, createRandomQuestions, getMissedQuestions, getPracticePlanQuestions, getQuestionBySectionAndIndex, getRandomQuestion } from "../Controller/PracticeplanController.js";
 
 
 
@@ -9,7 +9,9 @@ router.post('/CreatePraticePlan', createPraticePlan)
 router.post('/CheckAnswer/:id', checkAnswerById)
 router.get('/GetPracticePlanQuestions/:planId', getPracticePlanQuestions)
 router.get('/GetQuestion/:planId/:section/:questionIndex', getQuestionBySectionAndIndex)
-router.post('/GetRandomQuestions', getRandomQuestion)
+router.post('/CreateRandomQuestions',createRandomQuestions)
+router.post('/GetRandomQuestions',getRandomQuestion)
+router.post('/GetMissedQuestions',getMissedQuestions)
 
 
 export default router;
