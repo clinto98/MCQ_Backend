b78f8eb4a4d2127a3597a1db354a68a8cdf36a0b
clinto98 <clinto@finlytyx.com>
2025-09-29 19:08:50 +0530

    new-changes

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.use('/api/payment', PaymentRoute)
 app.use('/api/todaysquestion', TodaysQuestionRoute);
 
 
-app.listen(PORT, () => {
-    connectDB();
+app.listen(PORT, async () => {
+    await connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
 
